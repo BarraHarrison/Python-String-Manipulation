707bf00181bf6ce8a0148af55a9ffb8c9d413b9c
Barra Harrison <143306095+BarraHarrison@users.noreply.github.com>
2024-08-19 16:46:07 +0900

    Add files via upload

---
+++ name_manipulation.py
@@ -0,0 +1,10 @@
+first_name_input = input("Enter your first name: ")
+middle_name_input = input("Enter your middle name: ")
+last_name_input = input("Enter your last name: ")
+
+full_name = first_name_input + " " + middle_name_input + " " + last_name_input
+print(full_name)
+print(full_name[0:5])
+print(full_name[6:13])
+print(full_name[14:22])
+
